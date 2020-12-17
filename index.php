2bd18db609e93e0e13296ae4108fd1cdceee1432
alessio <alessio.cirillo.1995@gmail.com>
2020-12-17 15:57:07 +0100

    day one mileston done

--- index.php
+++ index.php
@@ -1,48 +1,5 @@
 <?php 
-    $database = [
-        [
-            'title' => 'New Jersey',
-            'author' => 'Bon Jovi',
-            'year' => 1988,
-            'poster' => 'https://images-na.ssl-images-amazon.com/images/I/51sBr4IWDwL.jpg',
-            'genre' => 'Rock'
-        ],
-        [
-            'title' => 'Live at Wembley 86',
-            'author' => 'Queen',
-            'year' => 1992,
-            'poster' => 'https://images-na.ssl-images-amazon.com/images/I/71g40mlbinL._SX355_.jpg',
-            'genre' => 'Pop'
-        ],
-        [
-            'title' => 'Ten\'s Summoner\'s Tales',
-            'author' => 'Sting',
-            'year' => 1993,
-            'poster' => 'https://images-na.ssl-images-amazon.com/images/I/411BQR6BHRL.jpg',
-            'genre' => 'Pop'
-        ],
-        [
-            'title' => 'Steve Gadd band',
-            'author' => 'Steve Gadd Band',
-            'year' => 2018,
-            'poster' => 'https://m.media-amazon.com/images/I/81UtLzBDoEL._SS500_.jpg',
-            'genre' => 'Jazz'
-        ],
-        [
-            'title' => 'And Justice for All',
-            'author' => 'Metallica',
-            'year' => 1988,
-            'poster' => 'https://images-na.ssl-images-amazon.com/images/I/81r3FVfNG3L._SY355_.jpg',
-            'genre' => 'Metal'
-        ],
-        [
-            'title' => 'One more car, one more rider',
-            'author' => 'Eric Clapton',
-            'year' => 2002,
-            'poster' => 'https://images-na.ssl-images-amazon.com/images/I/81MDAIdh78L._SY355_.jpg',
-            'genre' => 'Rock'
-        ]
-    ];
+    include './partials/database.php'
 ?>
 
 <!DOCTYPE html>
@@ -62,7 +19,19 @@
     </header>
 
     <main>
-        
+        <?php foreach( $database as $element){ ?>
+            <div class="poster">
+                <img src="<?php echo $element ["poster"] ?>" alt="">
+
+                <h3 class="title"><?php echo $element["title"]?></h3>
+
+                <small class="author"><?php echo $element["author"]?></small>
+
+                <h3 class="year"><?php echo $element["year"]?></h3>
+
+                <small class="genre"><?php echo $element["genre"]?></small>
+            </div>
+        <?php } ?>       
     </main>
 
 </body>
